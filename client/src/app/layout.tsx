c11377f2f8c44a7a7758c1feb2c722a6a18bfaf9
batahualpa11 <brianatahualpa@gmail.com>
2024-11-28 19:32:34 -0500

    updated layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const dmSans = DM_Sans ({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans ",
 });
 
 export const metadata: Metadata = {
